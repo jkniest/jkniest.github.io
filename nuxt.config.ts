11efafc1b5b3931788a51ac261eb3f398ca72af8
Jordan Kniest <mail@jkniest.de>
2025-07-26 15:40:24 +0200

    ci: Add linting & conventional commit checks (#2)
    * Add ducktory and linting github workflow
    
    * Finalize eslint config
    
    * Add commit.me config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
-    '@nuxt/icon'
-  ]
+    '@nuxt/icon',
+    '@therealironduck/ducktory',
+  ],
+
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
+
+  ducktory: {
+    debug: true,
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
