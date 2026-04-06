02d83130a66d55ab110eb665c7b83ee59cce1ae6
Jordan Kniest <mail@jkniest.de>
2026-04-06 10:14:18 +0200

    Remove eslint

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,13 @@
 <template>
-  <NuxtRouteAnnouncer />
+	<NuxtRouteAnnouncer />
 
-  <NuxtLayout>
-    <UApp>
-      <LayoutHeader />
-      <UMain class="bg-gradient-to-br from-gray-900 to-indigo-950">
-        <NuxtPage />
-      </UMain>
-      <LayoutFooter />
-    </UApp>
-  </NuxtLayout>
+	<NuxtLayout>
+		<UApp>
+			<LayoutHeader />
+			<UMain class="bg-linear-to-br from-gray-900 to-indigo-950">
+				<NuxtPage />
+			</UMain>
+			<LayoutFooter />
+		</UApp>
+	</NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,35 +2,28 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/ui',
-    '@therealironduck/ducktory',
-  ],
+	modules: [
+		'@nuxt/content',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/ui',
+		'@therealironduck/ducktory',
+	],
 
-  devtools: { enabled: true },
+	devtools: { enabled: true },
 
-  css: [
-    '~/assets/css/main.css',
-  ],
-  compatibilityDate: '2025-07-15',
+	css: [
+		'~/assets/css/main.css',
+	],
+	compatibilityDate: '2025-07-15',
 
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 
-  ducktory: {
-    debug: true,
-  },
-
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
+	ducktory: {
+		debug: true,
+	},
 })
